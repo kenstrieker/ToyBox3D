1dee8a7ba4014c55bb7d09b3744389f138217618
kenstrieker <kenstrieker@gmail.com>
2024-12-30 23:37:44 -0800

    adding index and staging buffers

--- model.cpp
+++ model.cpp
@@ -2,13 +2,19 @@
 #include <cassert>
 
 namespace engine {
-	model::model(device& deviceInstance, const std::vector<Vertex>& vertices) : deviceInstance{ deviceInstance } {
-		createVertexBuffers(vertices);
+	model::model(device& deviceInstance, const model::Builder& builderInstance) : deviceInstance{ deviceInstance } {
+		createVertexBuffers(builderInstance.vertices);
+		createIndexBuffer(builderInstance.indices);
 	}
 
 	model::~model() {
 		vkDestroyBuffer(deviceInstance.getDevice(), vertexBuffer, nullptr);
 		vkFreeMemory(deviceInstance.getDevice(), vertexBufferMemory, nullptr);
+
+		if (hasIndexBuffer) {
+			vkDestroyBuffer(deviceInstance.getDevice(), indexBuffer, nullptr);
+			vkFreeMemory(deviceInstance.getDevice(), indexBufferMemory, nullptr);
+		}
 	}
 
 	void model::createVertexBuffers(const std::vector<Vertex>& vertices) {
@@ -16,25 +22,69 @@ namespace engine {
 		vertexCount = static_cast<uint32_t>(vertices.size());
 		assert(vertexCount >= 3 && "Vertex count must be at least 3");
 
-		// create a vertex buffer
+		// create a staging buffer
 		VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount;
-		deviceInstance.createBuffer(bufferSize, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, vertexBuffer, vertexBufferMemory);
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
+		deviceInstance.createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
 		
-		// create a region of host memory mapped to device memory and set data to point to the beginning of the mapped memory range
+		// map the staging buffer memory
 		void* data;
-		vkMapMemory(deviceInstance.getDevice(), vertexBufferMemory, 0, bufferSize, 0, &data);
+		vkMapMemory(deviceInstance.getDevice(), stagingBufferMemory, 0, bufferSize, 0, &data);
 		memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
-		vkUnmapMemory(deviceInstance.getDevice(), vertexBufferMemory);
+		vkUnmapMemory(deviceInstance.getDevice(), stagingBufferMemory);
+
+		// create a vertex buffer
+		deviceInstance.createBuffer(bufferSize, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, vertexBuffer, vertexBufferMemory);
+		deviceInstance.copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+
+		vkDestroyBuffer(deviceInstance.getDevice(), stagingBuffer, nullptr);
+		vkFreeMemory(deviceInstance.getDevice(), stagingBufferMemory, nullptr);
+	}
+
+	void model::createIndexBuffer(const std::vector<uint32_t>& indices) {
+		// check that we are using an index buffer for rendering
+		indexCount = static_cast<uint32_t>(indices.size());
+		hasIndexBuffer = indexCount > 0;
+		if (!hasIndexBuffer) return;
+
+		// create a staging buffer
+		VkDeviceSize bufferSize = sizeof(indices[0]) * indexCount;
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
+		deviceInstance.createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+
+		// map the staging buffer memory
+		void* data;
+		vkMapMemory(deviceInstance.getDevice(), stagingBufferMemory, 0, bufferSize, 0, &data);
+		memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
+		vkUnmapMemory(deviceInstance.getDevice(), stagingBufferMemory);
+
+		// create a vertex buffer
+		deviceInstance.createBuffer(bufferSize, VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, indexBuffer, indexBufferMemory);
+		deviceInstance.copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+		vkDestroyBuffer(deviceInstance.getDevice(), stagingBuffer, nullptr);
+		vkFreeMemory(deviceInstance.getDevice(), stagingBufferMemory, nullptr);
 	}
 
 	void model::bind(VkCommandBuffer commandBuffer) {
 		VkBuffer buffers[] = { vertexBuffer };
 		VkDeviceSize offsets[] = { 0 };
 		vkCmdBindVertexBuffers(commandBuffer, 0, 1, buffers, offsets);
+
+		if (hasIndexBuffer) {
+			vkCmdBindIndexBuffer(commandBuffer, indexBuffer, 0, VK_INDEX_TYPE_UINT32);
+		}
 	}
 
 	void model::draw(VkCommandBuffer commandBuffer) {
-		vkCmdDraw(commandBuffer, vertexCount, 1, 0, 0);
+		if (hasIndexBuffer) {
+			vkCmdDrawIndexed(commandBuffer, indexCount, 1, 0, 0, 0);
+		}
+		else {
+			vkCmdDraw(commandBuffer, vertexCount, 1, 0, 0);
+		}
 	}
 
 	std::vector<VkVertexInputBindingDescription> model::Vertex::getBindingDescriptions() {
--- model.hpp
+++ model.hpp
@@ -16,7 +16,13 @@ namespace engine {
 			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
 		};
 
-		model(device& deviceInstance, const std::vector<Vertex>& vertices); // constructor
+		// struct for holding vertex and index information until it can be copied into the model's buffer memory
+		struct Builder {
+			std::vector<Vertex> vertices = {};
+			std::vector<uint32_t> indices = {};
+		};
+
+		model(device& deviceInstance, const model::Builder& builderInstance); // constructor
 		~model(); // destructor
 
 		// not copyable or movable
@@ -28,9 +34,14 @@ namespace engine {
 
 	private:
 		void createVertexBuffers(const std::vector<Vertex>& vertices); // to create the vertex buffers
+		void createIndexBuffer(const std::vector<uint32_t>& indices); // to create the index buffers
 		device& deviceInstance; // reference to the device
 		VkBuffer vertexBuffer; // a handle for the vertex buffer
 		VkDeviceMemory vertexBufferMemory; // a handle for the memory containing the vertex buffer
 		uint32_t vertexCount; // a handle for the count of vertices
+		bool hasIndexBuffer = false; // a flag for using index buffers
+		VkBuffer indexBuffer; // a handle for the index buffer
+		VkDeviceMemory indexBufferMemory; // a handle for the memory containing the index buffer
+		uint32_t indexCount; // a handle for the count of indices
 	};
 }
