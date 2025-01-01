d53668567192d0bb4db3b524d1967028ed797d9d
kenstrieker <kenstrieker@gmail.com>
2024-12-31 19:49:27 -0800

    adding model load functionality

--- model.cpp
+++ model.cpp
@@ -1,5 +1,22 @@
 #include "model.hpp"
+#include "utils.hpp"
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
 #include <cassert>
+#include <unordered_map>
+
+namespace std {
+	template <>
+	struct hash<engine::model::Vertex> {
+		size_t operator()(engine::model::Vertex const& vertex) const {
+			size_t seed = 0;
+			engine::hashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+			return seed;
+		}
+	};
+}
 
 namespace engine {
 	model::model(device& deviceInstance, const model::Builder& builderInstance) : deviceInstance{ deviceInstance } {
@@ -15,6 +32,12 @@ namespace engine {
 			vkDestroyBuffer(deviceInstance.getDevice(), indexBuffer, nullptr);
 			vkFreeMemory(deviceInstance.getDevice(), indexBufferMemory, nullptr);
 		}
+	}
+
+	std::unique_ptr<model> model::createModelFromFile(device& deviceInstance, const std::string& filepath) {
+		Builder builderInstance = {};
+		builderInstance.loadModel(filepath);
+		return std::make_unique<model>(deviceInstance, builderInstance);
 	}
 
 	void model::createVertexBuffers(const std::vector<Vertex>& vertices) {
@@ -108,5 +131,69 @@ namespace engine {
 		attributeDescriptions[1].offset = offsetof(Vertex, color);
 
 		return attributeDescriptions;
+	}
+
+	void model::Builder::loadModel(const std::string& filepath) {
+		tinyobj::attrib_t attrib;
+		std::vector<tinyobj::shape_t> shapes;
+		std::vector<tinyobj::material_t> materials;
+		std::string warn, err;
+
+		if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filepath.c_str())) {
+			throw std::runtime_error(warn + err);
+		}
+
+		// start from a fresh builder state
+		vertices.clear();
+		indices.clear();
+
+		std::unordered_map<Vertex, uint32_t> uniqueVertices = {};
+
+		for (const auto& shape : shapes) {
+			for (const auto& index : shape.mesh.indices) {
+				Vertex vertexInstance = {};
+
+				if (index.vertex_index >= 0) {
+					vertexInstance.position = { 
+						attrib.vertices[3 * index.vertex_index + 0],
+						attrib.vertices[3 * index.vertex_index + 1],
+						attrib.vertices[3 * index.vertex_index + 2],
+					};
+
+					auto colorIndex = 3 * index.vertex_index + 2;
+					if (colorIndex < attrib.colors.size()) {
+						vertexInstance.color = {
+							attrib.colors[colorIndex - 2],
+							attrib.colors[colorIndex - 1],
+							attrib.colors[colorIndex - 0],
+						};
+					}
+					else {
+						vertexInstance.color = { 1.f, 1.f, 1.f };
+					}
+				}
+
+				if (index.normal_index >= 0) {
+					vertexInstance.normal = {
+						attrib.vertices[3 * index.normal_index + 0],
+						attrib.vertices[3 * index.normal_index + 1],
+						attrib.vertices[3 * index.normal_index + 2],
+					};
+				}
+
+				if (index.texcoord_index >= 0) {
+					vertexInstance.uv = {
+						attrib.texcoords[2 * index.texcoord_index + 0],
+						attrib.texcoords[2 * index.texcoord_index + 1],
+					};
+				}
+
+				if (uniqueVertices.count(vertexInstance) == 0) {
+					uniqueVertices[vertexInstance] = static_cast<uint32_t>(vertices.size());
+					vertices.push_back(vertexInstance);
+				}
+				indices.push_back(uniqueVertices[vertexInstance]);
+			}
+		}
 	}
 }
--- model.hpp
+++ model.hpp
@@ -4,22 +4,29 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 #include <vector>
+#include <memory>
 
 namespace engine {
 	class model {
 	public:
 		// struct for vertex attributes to make them easier to work with
 		struct Vertex {
-			glm::vec3 position;
-			glm::vec3 color;
+			glm::vec3 position = {};
+			glm::vec3 color = {};
+			glm::vec3 normal = {};
+			glm::vec2 uv = {};
 			static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
 			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+			bool operator==(const Vertex& other) const {
+				return position == other.position && color == other.color && normal == other.normal && uv == other.uv;
+			}
 		};
 
 		// struct for holding vertex and index information until it can be copied into the model's buffer memory
 		struct Builder {
 			std::vector<Vertex> vertices = {};
 			std::vector<uint32_t> indices = {};
+			void loadModel(const std::string& filepath);
 		};
 
 		model(device& deviceInstance, const model::Builder& builderInstance); // constructor
@@ -28,6 +35,8 @@ namespace engine {
 		// not copyable or movable
 		model(const model&) = delete;
 		model& operator = (const model&) = delete;
+
+		static std::unique_ptr<model> createModelFromFile(device& deviceInstance, const std::string& filepath);
 
 		void bind(VkCommandBuffer commandBuffer);
 		void draw(VkCommandBuffer commandBuffer);
